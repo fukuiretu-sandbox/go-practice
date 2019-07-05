0595582ac076e4e905bb2638fdfe72f7acfa45ac
fukuiretu <fukuiretu@gmail.com>
2019-07-05 10:35:23 +0900

    add

--- syntax/main.go
+++ syntax/main.go
@@ -15,8 +15,23 @@ func (d Dice) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
 
 var dice = Dice([]int{1, 2, 3, 4, 5, 6})
 
+type foo struct {
+	v int
+}
+
+func (f *foo) add(v int) {
+	f.v += v
+}
+
 func main() {
 	fmt.Printf("%v\n", dice)
 	shuffle.Shuffle(dice)
 	fmt.Printf("%v\n", dice)
+
+	// NOTE: ポインタの例
+	var a foo
+	a.add(3)
+	println("a.v:", a.v)
+	a.add(4)
+	println("a.v:", a.v)
 }
